5c7b15e6b4369dc2d7a9e8e3346e716c17701ee0
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-08-17 08:28:55 -0500

    passing tests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
-  
+    const inputForm = 
+    document.querySelector('form')
+    inputForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const input = document.querySelector("#searchByID")
+
+        console.log(input.value);
+        const source = `http://localhost:3000/movies/${input.value}`
+
+        fetch(source)
+        .then(result => result.json())
+        .then(data => { 
+                console.log(data)
+        })
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
